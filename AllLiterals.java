f67a5d13f234fcf99906ee68832d2af42d83c53c
Husnain-112 <105660928+Husnain-112@users.noreply.github.com>
2022-05-30 12:23:21 +0500

    Add files via upload

---
+++ AllLiterals.java
@@ -0,0 +1,40 @@
+public class  AllLiterals{
+
+public static void main(String args[]){
+
+int dec=300;
+int oct=0577;
+int hex=0x342E;
+int bin=0b10010;
+long lng=3_456_789L;
+float flt=49.346f;
+double dbl=2345.235e-4;
+double hex_dbl=0x2364.234p-8;
+char ch='D';
+String str="All Literal Program";
+
+
+System.out.println("deciimal dec is " + dec);
+System.out.println("octal oct is " + oct);
+System.out.println("hexa-decimal hex is " + hex);
+System.out.println("binary bin is " + bin);
+System.out.println("long lng is " + lng);
+System.out.println("float flt is " + flt);
+System.out.println("double dbl is " + dbl);
+System.out.println("hexa-decimal float hex_dbl is " + hex_dbl);
+System.out.println("char ch is " + ch);
+System.out.println("String str is " + str);
+
+
+
+
+
+}
+
+
+
+
+
+
+
+} 
---
+++ EscSeq.java
@@ -0,0 +1,20 @@
+public class EscSeq {
+
+public static void main(String args[]){
+
+System.out.println("use of Escape Sequences ");
+System.out.println("use of \\ddd for octal representation: ");
+System.out.println("\\141 = \141 ");
+
+System.out.println("use of \\uxxxx for hexadecimal representation:  ");
+System.out.println("\\u0061 = \u0061 ");
+
+System.out.println("use of other Escape Sequences ");
+System.out.println("\t\'Program\'\n\t\"Hello World\\Earth \"  ");
+
+
+
+
+}
+
+} 
